8904ae5ce75b68e0b798076c838ab82bf2d07482
bananovblu <barapcmax@gmail.com>
2018-10-07 03:51:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var image = document.getElementById('photo');
+    image.onclick = function(){
+        this.style.width = '600px';
+        this.style.height = '600px';
+
+    }
+};
